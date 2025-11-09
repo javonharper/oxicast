d2bca0fa79d7831deb2e7b5a0b91e47f0c1aab57
Javon Harper <javon.d.harper@gmail.com>
2025-11-09 17:16:49 -0500

    Add server

---
+++ src/server.rs
@@ -0,0 +1,30 @@
+use axum::{
+    Router,
+    routing::{get, get_service},
+};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+pub async fn serve(root_dir: &str) {
+    let app = Router::new()
+        .route("/", get(|| async { "Ok!" }))
+        .nest_service(
+            "/shows",
+            get_service(ServeDir::new(root_dir)).handle_error(|error| async move {
+                println!("Error serving directory: {}", error);
+                (
+                    axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                    "Something went wrong",
+                )
+            }),
+        );
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+
+    println!("Serving directory {}", root_dir);
+    println!("Serving on http://127.0.0.1:8080/shows");
+
+    axum::serve(listener, app).await.unwrap();
+}
