aafaf1471df0200667bb4ab79456a69ce6eb32d7
Javon Harper <javon.d.harper@gmail.com>
2025-11-09 18:41:15 -0500

    Update server.rs

--- src/server.rs
+++ src/server.rs
@@ -5,8 +5,6 @@ use axum::{
 
 use tower_http::services::ServeDir;
 
-use crate::network::get_host_ip;
-
 #[tokio::main]
 pub async fn serve(root_dir: &str) {
     let app = Router::new()
@@ -25,7 +23,7 @@ pub async fn serve(root_dir: &str) {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     // println!("Serving directory {}", root_dir);
-    let ip = get_host_ip().expect("Failed to get local IP");
+    // let ip = get_host_ip().expect("Failed to get local IP");
     // println!("Serving on http://{}:8080/shows", ip);
 
     axum::serve(listener, app).await.unwrap();
